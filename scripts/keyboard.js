1625b5bef485e3bc2f436997b59fa06b26ed22ce
Joao Pedro Machado <joaopemachado2200@gmail.com>
2022-09-02 14:25:13 -0300

    fix keyboard after win/lose

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,17 +1,23 @@
 import {game, updateGame} from './game.js'
 
+const div = document.querySelector(".secret-word-content");
+
 export function keyboard() {
     document.addEventListener("keydown", (event) => {
-        const key = event.keyCode; // 65 - 90 (Intervalo)
-        
+        let key = event.keyCode; // 65 - 90 (Intervalo)
+
         if(isLetter(key)){
             const LETTER = event.key.toUpperCase();
             if (game.wrongLetter.includes(LETTER)) {
             } else {
-                if(game.word.includes(LETTER)){
-                    game.correctLetter.push(LETTER);
-                } else {
-                    game.wrongLetter.push(LETTER);
+                if(game.wrongLetter.length === 7 || div.innerText === game.word){
+                    key = false;
+                }else {
+                    if(game.word.includes(LETTER)){
+                        game.correctLetter.push(LETTER);
+                    } else {
+                        game.wrongLetter.push(LETTER);
+                    }
                 }
             }
             updateGame();
